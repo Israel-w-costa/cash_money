d22205e96fbb0cd8d2ed0cc46dccc8760e78782f
israel <israelwendellc@gmail.com>
2023-07-28 20:43:46 -0300

    adicionando o botao de delete

--- js/main.js
+++ js/main.js
@@ -34,12 +34,35 @@ function criarElemento (item) {
     novoItem.classList.add ("lista__item")
     const novoEfeito =document.createElement ('strong')
     novoEfeito.classList.add ("lista__forte")
+    novoEfeito.dataset = item.id
     novoEfeito.innerHTML = "R$ " + item.preco
 
     novoItem.innerHTML += item.nome
     novoItem.appendChild (novoEfeito)
+    novoItem.appendChild (botaoDeleta(item.id))
 
     lista.appendChild (novoItem)
     
 
+}
+
+function botaoDeleta (id) {
+    const deleta = document.createElement ("button")
+    deleta.innerText = "X"
+
+    deleta.addEventListener ("click", function (tag) {
+    deleteBotao (this.parentNode)
+        
+
+    })
+
+    return deleta
+} 
+
+function deleteBotao (tag, id) {
+    itens.splice ( itens.find (elemento => elemento.id ),1)    
+    localStorage.setItem ("itens",JSON.stringify (itens))
+
+    tag.remove ()
+    
 }
